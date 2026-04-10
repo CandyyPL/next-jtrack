c259af060ea4eea91017df48301348cc3671f59c
CandyyPL <aplay58.yt@gmail.com>
2026-04-10 17:07:09 +0200

    refactor navbar

---
+++ components/navbar-buttons.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { signOut, useSession } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+export default function NavbarButtons() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut();
+    router.push('/');
+  };
+
+  return (
+    <div className='flex items-center gap-4'>
+      {session?.user ? (
+        <>
+          <Link href='/dashboard'>
+            <Button
+              variant='ghost'
+              className='text-gray-700 hover:text-black'>
+              Dashboard
+            </Button>
+          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button
+                variant='ghost'
+                className='relative h-10 w-10 rounded-full'>
+                <Avatar className='h-10 w-10'>
+                  <AvatarFallback className='bg-primary text-white'>
+                    {session?.user.name.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className='w-42'
+              align='center'>
+              <DropdownMenuLabel className='font-normal'>
+                <div className='flex flex-col gap-1 space-y-1'>
+                  <p className='text-sm leading-none font-medium'>
+                    {session?.user.name}
+                  </p>
+                  <p className='text-muted-foreground text-xs leading-none'>
+                    {session?.user.email}
+                  </p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => handleLogout()}
+                className='cursor-pointer'>
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
+      ) : (
+        <>
+          <Link href='/sign-in'>
+            <Button
+              variant='ghost'
+              className='text-gray-700 hover:text-black'>
+              Login
+            </Button>
+          </Link>
+          <Link href='/sign-up'>
+            <Button className='hover:bg-primary/90'>Start for free</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,8 @@
-import { Button } from '@/components/ui/button';
 import { Briefcase } from 'lucide-react';
 import Link from 'next/link';
-import { getSession } from '@/lib/auth';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import NavbarLogout from '@/components/navbar-logout';
+import NavbarButtons from '@/components/navbar-buttons';
 
 export default async function Navbar() {
-  const session = await getSession();
-
   return (
     <nav className='border-b border-gray-200'>
       <div className='container mx-auto flex h-16 items-center justify-between px-4'>
@@ -25,54 +14,7 @@ export default async function Navbar() {
             <span className='text-primary'>J</span>Track
           </h1>
         </Link>
-        <div className='flex items-center gap-4'>
-          {session?.user ? (
-            <>
-              <Link href='/dashboard'>
-                <Button
-                  variant='ghost'
-                  className='text-gray-700 hover:text-black'>
-                  Dashboard
-                </Button>
-              </Link>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <div>
-                    <Avatar>
-                      <AvatarFallback className='bg-primary font-medium text-white'>
-                        {session?.user.name.charAt(0).toUpperCase()}
-                      </AvatarFallback>
-                    </Avatar>
-                  </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  className='w-fit'
-                  align='center'>
-                  <DropdownMenuLabel>
-                    <div>
-                      <p>{session?.user.name}</p>
-                      <p>{session?.user.email}</p>
-                    </div>
-                  </DropdownMenuLabel>
-                  <NavbarLogout />
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </>
-          ) : (
-            <>
-              <Link href='/sign-in'>
-                <Button
-                  variant='ghost'
-                  className='text-gray-700 hover:text-black'>
-                  Login
-                </Button>
-              </Link>
-              <Link href='/sign-up'>
-                <Button className='hover:bg-primary/90'>Start for free</Button>
-              </Link>
-            </>
-          )}
-        </div>
+        <NavbarButtons />
       </div>
     </nav>
   );
