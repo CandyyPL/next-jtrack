d7e1a2b098c945ab707d6f46c9aad2b10ff7850c
CandyyPL <aplay58.yt@gmail.com>
2026-04-22 22:59:59 +0200

    add board query with kanban board to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,37 @@
-export default function Dashboard() {
-  return <p>Dashboard page</p>;
+import { supabase } from '@/lib/supabase';
+import { getSession } from '@/lib/auth';
+import { Board } from '@/lib/types';
+import KanbanBoard from '@/components/kanban-board';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect('/sign-in');
+  }
+
+  const { data: rawData } = await supabase
+    .from('board')
+    .select()
+    .eq('userId', session.user.id)
+    .single();
+
+  const board = rawData as Board;
+
+  return (
+    <main className='min-h-[calc(100vh-4rem-1px)]'>
+      <div className='container mx-auto p-6'>
+        <header className='mb-6'>
+          <h1 className='text-3xl font-bold text-black'>{board.name}</h1>
+          <p className='text-gray-600'>Track your job applications.</p>
+        </header>
+
+        <KanbanBoard
+          board={board}
+          userId={session.user.id}
+        />
+      </div>
+    </main>
+  );
 }
