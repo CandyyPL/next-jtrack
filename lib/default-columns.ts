cae6b82888cdb55703b57594922bf8e6e2e42baf
CandyyPL <aplay58.yt@gmail.com>
2026-04-22 17:59:35 +0200

    add initializing columns in new user board

---
+++ lib/default-columns.ts
@@ -0,0 +1,27 @@
+export const DEFAULT_COLUMNS = [
+  {
+    name: 'Wish List',
+    listOrder: 0,
+    boardId: '',
+  },
+  {
+    name: 'Applied',
+    listOrder: 1,
+    boardId: '',
+  },
+  {
+    name: 'Interviewing',
+    listOrder: 2,
+    boardId: '',
+  },
+  {
+    name: 'Offer',
+    listOrder: 3,
+    boardId: '',
+  },
+  {
+    name: 'Rejected',
+    listOrder: 4,
+    boardId: '',
+  },
+];
---
+++ lib/init-user-board.ts
@@ -0,0 +1,42 @@
+import { supabase } from '@/lib/supabase';
+import { Board } from '@/lib/types';
+import { DEFAULT_COLUMNS } from '@/lib/default-columns';
+
+async function initColumns(boardId: string) {
+  const columns = DEFAULT_COLUMNS.map((column) => ({
+    ...column,
+    boardId,
+  }));
+
+  await supabase.from('column').insert(columns);
+}
+
+export async function initUserBoards(userId: string) {
+  const { data: rawExisting } = await supabase
+    .from('board')
+    .select()
+    .eq('userId', userId)
+    .eq('name', 'Job Hunt')
+    .maybeSingle();
+
+  const existingBoard = rawExisting as Board;
+
+  if (existingBoard) {
+    await initColumns(existingBoard.id);
+    return;
+  }
+
+  const board = {
+    name: 'Job Hunt',
+    userId,
+  };
+
+  const { data: rawSaved } = await supabase
+    .from('board')
+    .insert(board)
+    .select()
+    .single();
+
+  const saved = rawSaved as Board;
+  await initColumns(saved.id);
+}
