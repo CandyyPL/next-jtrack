0c8c50385951e38344f9b3a6d9e2348b40a3b0d3
CandyyPL <aplay58.yt@gmail.com>
2026-04-23 13:56:49 +0200

    create main page dir

--- components/main-page/hero-image-gallery.tsx
+++ components/main-page/hero-image-gallery.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/shadcn/button';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { Activity, useState } from 'react';
