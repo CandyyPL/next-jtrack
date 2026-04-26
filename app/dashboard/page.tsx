0458608651498cae4a532e137fe1b28bc71e0562
CandyyPL <aplay58.yt@gmail.com>
2026-04-26 12:47:32 +0200

    query database for full board data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,46 @@
 import { supabase } from '@/lib/supabase';
 import { getSession } from '@/lib/auth';
-import { Board } from '@/lib/types';
+import { Application, Board, Column, FullBoardData } from '@/lib/types';
 import KanbanBoard from '@/components/kanban/kanban-board';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
-async function getBoard(userId: string) {
+async function getData(userId: string) {
   'use cache';
 
-  const { data: rawData } = await supabase
+  const { data: rawBoard } = await supabase
     .from('board')
     .select()
     .eq('userId', userId)
     .single();
 
-  return rawData as Board;
+  const board = rawBoard as Board;
+
+  const { data: rawColumns } = await supabase
+    .from('column')
+    .select()
+    .eq('boardId', board.id);
+
+  const columns = rawColumns as Column[];
+
+  const { data: rawApplications } = await supabase
+    .from('application')
+    .select()
+    .in(
+      'columnId',
+      columns.map((col) => col.id)
+    );
+
+  const applications = rawApplications as Application[];
+
+  const populatedColumns = columns.map((col) => ({
+    ...col,
+    applications: applications.filter((job) => job.columnId === col.id),
+  }));
+
+  const fullBoardData: FullBoardData = { ...board, columns: populatedColumns };
+
+  return fullBoardData;
 }
 
 async function DashboardWrapper() {
@@ -24,17 +50,17 @@ async function DashboardWrapper() {
     redirect('/sign-in');
   }
 
-  const board = await getBoard(session.user.id);
+  const boardData = await getData(session.user.id);
 
   return (
     <main className='min-h-[calc(100vh-4rem-1px)]'>
       <div className='container mx-auto p-6'>
         <header className='mb-6'>
-          <h1 className='text-3xl font-bold text-black'>{board.name}</h1>
+          <h1 className='text-3xl font-bold text-black'>{boardData.name}</h1>
           <p className='text-gray-600'>Track your job applications.</p>
         </header>
         <KanbanBoard
-          board={board}
+          boardData={boardData}
           userId={session.user.id}
         />
       </div>
