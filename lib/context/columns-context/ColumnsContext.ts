e797435aa8da2931399d4ef4e9d45fce3cedcaeb
CandyyPL <aplay58.yt@gmail.com>
2026-04-28 12:54:06 +0200

    remove unused import

--- lib/context/columns-context/ColumnsContext.ts
+++ lib/context/columns-context/ColumnsContext.ts
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 import { Application, ColumnWithApplication, Optional } from '@/lib/types';
 
 export type ColumnsContext = {
