8a12f5ea1748ca95c46db19f663d220d5a2e3efc
CandyyPL <aplay58.yt@gmail.com>
2026-04-29 20:54:59 +0200

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,12 @@ export default function RootLayout({
       <body className='flex min-h-full flex-col'>
         <Navbar />
         {children}
+        <footer className='flex h-30 flex-col items-center justify-center bg-zinc-100 shadow-lg inset-shadow-sm'>
+          <p className='text-xl font-semibold'>JTrack &copy; 2026</p>
+          <p className='text-muted-foreground text-sm'>
+            Start tracking you job application today!
+          </p>
+        </footer>
       </body>
     </html>
   );
