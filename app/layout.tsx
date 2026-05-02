bc046e5870fa1ee342d7d3c84760eb4e501fa311
CandyyPL <aplay58.yt@gmail.com>
2026-05-02 17:50:40 +0200

    extract footer to external file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Geist, Lusitana } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import Navbar from '@/components/navbar/navbar';
+import Footer from '@/components/footer';
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -40,12 +41,7 @@ export default function RootLayout({
       <body className='content-height flex flex-col'>
         <Navbar />
         {children}
-        <footer className='flex h-30 flex-col items-center justify-center bg-zinc-100 shadow-lg inset-shadow-sm'>
-          <p className='text-xl font-semibold'>JTrack &copy; 2026</p>
-          <p className='text-muted-foreground text-sm'>
-            Start tracking you job application today!
-          </p>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
