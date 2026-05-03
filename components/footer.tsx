6860743b0eddfcb2aa07b30cdeec99aba2cbf518
CandyyPL <aplay58.yt@gmail.com>
2026-05-03 20:47:21 +0200

    fix typo in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className='flex h-30 flex-col items-center justify-center bg-zinc-100 shadow-lg inset-shadow-sm'>
       <p className='text-xl font-semibold'>JTrack &copy; 2026</p>
       <p className='text-muted-foreground text-sm'>
-        Start tracking you job application today!
+        Start tracking your job application today!
       </p>
     </footer>
   );
